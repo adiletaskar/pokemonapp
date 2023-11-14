9b13a2273fc1e2d77bbbc10daaf54459b035d66b
adiletaskar <adiletaskar1@gmail.com>
2023-11-15 00:06:14 +0600

    jwt security

---
+++ src/main/java/com/pokemonreview/api/models/Role.java
@@ -0,0 +1,18 @@
+package com.pokemonreview.api.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+}
